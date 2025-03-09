d2fd95aa21576468175fce550eeafd3fd65c7f60
Michael O'Hara <michael.ohara@hotmail.co.nz>
2025-03-10 10:05:52 +1300

    none

---
+++ auth.bat
@@ -0,0 +1,2 @@
+git config user.email "michael.ohara@hotmail.co.nz"
+git config user.password "Happywalnut99!"
--- sync.bat
+++ sync.bat
@@ -1,5 +1,3 @@
-git config user.email "michael.ohara@hotmail.co.nz"
-git config user.password "Happywalnut99!"
 git add .
 @echo off
 set /p "msg=Message: "
