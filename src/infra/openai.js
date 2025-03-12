21d2a1b76fc7dcafeaa4acf5530e896d44a8b846
Michael O'Hara <michael.ohara@hotmail.co.nz>
2025-03-12 17:10:25 +1300

    things

--- src/infra/openai.js
+++ src/infra/openai.js
@@ -6,6 +6,6 @@ export const openai = new OpenAI({
     organization: "org-cEC4INxsgMtKfNh3z6NItl8m",
     project: "proj_GTKrQcUnol2uPXmpqbJvcVEF",
     apiKey:
-      "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",
+      "",
     dangerouslyAllowBrowser: true,
   });
