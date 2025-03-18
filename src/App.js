97813ec09e200d67b6ad5ea5f5650f3f1bbe934b
Michael O'Hara <michael.ohara@hotmail.co.nz>
2025-03-19 08:42:20 +1300

    rules

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import WordList from "./pages/wordList";
 
 function App() {
   const [error, setError] = useState();
+  const [rulesOpen, setRulesOpen] = useState();
   const [page, setPage] = useState('practice');
+
+  console.log(rulesOpen);
 
   function getHeaderText(){
     if (error)
@@ -28,8 +31,10 @@ function App() {
     <div className="App">
       <div className={`content-row header ${error ? 'error-banner' : ''}`}>
         <p>{getHeaderText()}</p>
-        {error && <div className="pointer" style={{fontSize:24}} onClick={() => setError("")}>x</div>}
+        {error && <div className="header-control" onClick={() => setError("")}>X</div>}
+        {!error && <div className="header-control" onClick={() => setRulesOpen(true)}>i</div>}
       </div>
+      <Modal open={rulesOpen} toggle={setRulesOpen}/>
       <WordList reportError={setError} isActive={page === 'wordlist'}/>
       <Practice reportError={setError} isActive={page === 'practice'}/>
       <Chat reportError={setError} isActive={page === 'chat'}/>
@@ -43,3 +48,50 @@ function App() {
 }
 
 export default App;
+
+function Modal(props){
+  if (!props.open)
+    return;
+
+  return <div className="modal">
+    <div className={`content-row header`}>
+      <p>Rules and Info</p>
+      <div className="header-control" onClick={() => props.toggle(false)}>X</div>
+    </div>
+    <div className="modal-body">
+      <div className={`content-row`}>
+        <p>Present continuous</p>
+      </div>
+      <div className={`content-row`}>
+        <p>I am eating:</p>
+        <p>Estoy comiendo</p>
+      </div>
+      <div className={`content-row`}>
+        <p>I am thinking:</p>
+        <p>Estoy pensando</p>
+      </div>
+      <div className={`content-row`}>
+        <p>Present perfect</p>
+      </div>
+      <div className={`content-row`}>
+        <p>I have eaten:</p>
+        <p>He comido</p>
+      </div>
+      <div className={`content-row`}>
+        <p>I have thought:</p>
+        <p>He pensado</p>
+      </div>
+      <div className={`content-row`}>
+        <p>Future simple</p>
+      </div>
+      <div className={`content-row`}>
+        <p>I will eat:</p>
+        <p>Comeré</p>
+      </div>
+      <div className={`content-row`}>
+        <p>I will think:</p>
+        <p>Pensaré</p>
+      </div>
+    </div>
+  </div>
+}
